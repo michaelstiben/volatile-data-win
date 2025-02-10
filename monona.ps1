fc06dd40d47fa988cf7b4fe1aa1c6a1b61f870c0
Michael Paredes <michael.paredes@geniussports.com>
2025-02-10 02:30:01 -0500

    add volatility commands

--- monona.ps1
+++ monona.ps1
@@ -105,18 +105,15 @@ function Volatility-Recopilation{
     Write-Host "No se ha seleccionado un archivo .raw terminando programa." -ForegroundColor Red
     return 0
   }
-  
-  $imageInfoCommand = "imageinfo"
-  $imageInfoArguments = @($imageInfoCommand, "-f", $rawFilePath)
-
-  $imageInfoOutput = & $volatilityExePath @imageInfoArguments
 
   $imageProfileRegex = "(VistaSP[0-2]x(64|86)|Win(10|2003SP[0-2]|2008(R2)?SP[0-2]|2012R2|2012|7SP[0-1]|81U1|8SP[0-1]|XP(SP[1-3])?)x(64|86))"
+  $physicalOffsetRegex = "Offset \(V\)\s+:\s+(0x[0-9a-fA-F]+)"
+
+  $imageInfoArguments = @("imageinfo", "-f", $rawFilePath)
+  $imageInfoOutput = & $volatilityExePath @imageInfoArguments
 
   $profileMatches = $imageInfoOutput | Select-String -Pattern $imageProfileRegex -AllMatches | Select-Object -First 1
-
   $volatilityProfile = $profileMatches.Matches | Select-Object -First 1
-
   Write-Host "Perfil encontrado: $volatilityProfile"
   $imageInfoFilePath = $DirectoryPath + "\imageinfo.txt"
   $imageInfoOutput | Out-File -FilePath $imageInfoFilePath -Encoding UTF8
@@ -125,6 +122,62 @@ function Volatility-Recopilation{
   $kdbgscanOutput = & $volatilityExePath @kdbgscanArguments
   $kdbgscanFilePath = $DirectoryPath + "\kdbgscan.txt"
   $kdbgscanOutput | Out-File -FilePath $kdbgscanFilePath -Encoding UTF8
+
+  
+  $physicalOffsetMatches = $kdbgscanOutput | Select-String -Pattern $physicalOffsetRegex 
+  $physicalOffset = $physicalOffsetMatches.Matches.Groups[1].Value
+  Write-Host "Offset fisico encontrado: $physicalOffset"
+
+  $psListArguments = @("pslist", "-f", $rawFilePath, "--profile", $volatilityProfile, "--kdbg", $physicalOffset)
+  $psListOutput = & $volatilityExePath @psListArguments
+  $psListFilePath = $DirectoryPath + "\psList.txt"
+  $psListOutput | Out-File -FilePath $psListFilePath -Encoding UTF8
+
+  $dllListArguments = @("dlllist", "-f", $rawFilePath, "--profile", $volatilityProfile, "--kdbg", $physicalOffset)
+  $dllListOutput = & $volatilityExePath @dllListArguments
+  $dllListFilePath = $DirectoryPath + "\dllList.txt"
+  $dllListOutput | Out-File -FilePath $dllListFilePath -Encoding UTF8
+
+  $handlesArguments = @("handles", "-f", $rawFilePath, "--profile", $volatilityProfile, "--kdbg", $physicalOffset)
+  $handlesOutput = & $volatilityExePath @handlesArguments
+  $handlesFilePath = $DirectoryPath + "\handles.txt"
+  $handlesOutput | Out-File -FilePath $handlesFilePath -Encoding UTF8
+
+  $mftparserArguments = @("mftparser", "-f", $rawFilePath, "--profile", $volatilityProfile, "--kdbg", $physicalOffset)
+  $mftparserOutput = & $volatilityExePath @mftparserArguments
+  $mftparserFilePath = $DirectoryPath + "\mftparser.txt"
+  $mftparserOutput | Out-File -FilePath $mftparserFilePath -Encoding UTF8
+
+  $timelinerArguments = @("timeliner", "-f", $rawFilePath, "--profile", $volatilityProfile, "--kdbg", $physicalOffset)
+  $timelinerOutput = & $volatilityExePath @timelinerArguments
+  $timelinerFilePath = $DirectoryPath + "\timeliner.txt"
+  $timelinerOutput | Out-File -FilePath $timelinerFilePath -Encoding UTF8
+
+  $cmdscanArguments = @("cmdscan", "-f", $rawFilePath, "--profile", $volatilityProfile, "--kdbg", $physicalOffset)
+  $cmdscanOutput = & $volatilityExePath @cmdscanArguments
+  $cmdscanFilePath = $DirectoryPath + "\cmdscan.txt"
+  $cmdscanOutput | Out-File -FilePath $cmdscanFilePath -Encoding UTF8
+
+  $consolesArguments = @("consoles", "-f", $rawFilePath, "--profile", $volatilityProfile, "--kdbg", $physicalOffset)
+  $consolesOutput = & $volatilityExePath @consolesArguments
+  $consolesFilePath = $DirectoryPath + "\consoles.txt"
+  $consolesOutput | Out-File -FilePath $consolesFilePath -Encoding UTF8
+
+  $hivescanArguments = @("hivescan", "-f", $rawFilePath, "--profile", $volatilityProfile, "--kdbg", $physicalOffset)
+  $hivescanOutput = & $volatilityExePath @hivescanArguments
+  $hivescanFilePath = $DirectoryPath + "\hivescan.txt"
+  $hivescanOutput | Out-File -FilePath $hivescanFilePath -Encoding UTF8
+
+  $procexedumpFilePath = $DirectoryPath + "\procexedump"
+  Ensure-Directory -DirectoryPath $procexedumpFilePath
+  $procexedumpArguments = @("procexedump", "-f", $rawFilePath, "--profile", $volatilityProfile, "--kdbg", $physicalOffset, "--dump-dir", $procexedumpFilePath)
+  $procexedumpOutput = & $volatilityExePath @procexedumpArguments
+  
+  $dlldumpFilePath = $DirectoryPath + "\dlldump"
+  Ensure-Directory -DirectoryPath $dlldumpFilePath
+  $dlldumpArguments = @("dlldump", "-f", $rawFilePath, "--profile", $volatilityProfile, "--kdbg", $physicalOffset, "--dump-dir", $dlldumpFilePath)
+  $dlldumpOutput = & $volatilityExePath @dlldumpArguments
+
 }
 
 $volatilityExePath = ".\volatility.exe"
